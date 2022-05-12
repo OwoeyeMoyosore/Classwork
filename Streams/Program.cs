7ab953b796136554d66c6fd41c70864722514bfd
Owoeye Moyosore <moyosoreowoeye2000@gmail.com>
2022-05-12 15:51:27 +0100

    Streams

--- Streams/Program.cs
+++ Streams/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 //using System.IO;
 namespace Fileinfo
 {
@@ -16,32 +17,93 @@ namespace Fileinfo
             Console.WriteLine(dir.Name);
             Console.WriteLine(dir.FullName);
 
-            //File
-            string[] words = { "This is a ball", "This is a house", "Set to Chalk", "micess" };
-            string path = @"C:\Users\user\new text document.txt";
-            string path2 = @"C:\Users\user\testttt.docx";
-            File.WriteAllLines(path, words);
-            File.WriteAllLines(path2, words);
+            ////File
+            //string[] words = { "This is a ball", "This is a house", "Set to Chalk", "micess" };
+            //string path = @"C:\Users\user\new text document.txt";
+            //string path2 = @"C:\Users\user\testttt.docx";
+            //File.WriteAllLines(path, words);
+            //File.WriteAllLines(path2, words);
 
-            foreach(var item in File.ReadAllLines(path))
-            {
-                Console.WriteLine(item);
-            }
+            //foreach(var item in File.ReadAllLines(path))
+            //{
+            //    Console.WriteLine(item);
+            //}
             //foreach (var item in File.ReadAllLines(pathss))
             //{
             //    Console.WriteLine(item);
             //}
 
             //Fileinfo
-            string path3 = @"C:\Users\user\new text document.txt";
-            DirectoryInfo dirr = new DirectoryInfo(@"C:\Users\user\OneDrive\Desktop\ui");
-            FileInfo [] textfile = dirr.GetFiles("*.jpg",SearchOption.AllDirectories);
-            Console.WriteLine("matches: {0}", textfile.Length);
-            foreach(var item in textfile)
+            //string path3 = @"C:\Users\user\new text document.txt";
+            //DirectoryInfo dirr = new DirectoryInfo(@"C:\Users\user\OneDrive\Desktop\ui");
+            //FileInfo [] textfile = dirr.GetFiles("*.jpg",SearchOption.AllDirectories);
+            //Console.WriteLine("matches: {0}", textfile.Length);
+            //foreach(var item in textfile)
+            //{
+            //    Console.WriteLine(item.FullName);
+            //    Console.WriteLine(item.CreationTime);
+            //}
+
+            //Writeforall();
+            Readforall();
+
+            //Filestream WRITE
+            string path4 = @"C:\Users\user\New Text Document.txt";
+            var fs = new FileStream(path4, FileMode.Open);
+                    //FileStream fs = File.Open(path4, FileMode.Create);
+            string newword = "this is a brand new carssss";
+
+            //When you want to write, you convert to byte
+            byte[] writing = Encoding.Default.GetBytes(newword);
+            fs.Write(writing, 0, writing.Length);
+            //fs.Close();
+            fs.Position = 0;
+           
+            
+            //fs.Close();
+            //FILESTREAM READ
+            //byte[] reading = new byte[writing.Length];
+            //for (int i = 0; i < writing.Length; i++)
+            //{
+            //    reading[i] = (byte)fs.ReadByte();
+            //}
+            //Console.WriteLine(Encoding.Default.GetString(reading));
+            //fs.Close();
+
+            int j = 0;
+            while ((j = fs.ReadByte()) != -1)
             {
-                Console.WriteLine(item.FullName);
-                Console.WriteLine(item.CreationTime);
+                Console.Write((char)j);
             }
+            fs.Close();
+
         }
+
+
+    
+         
+        //Stream Writer
+        //public static void Writeforall()
+        //{
+           
+        //    StreamWriter sw = new StreamWriter(@"C:\Users\user\New Text Document.txt");
+        //    sw.WriteLine("welcome to cyberspace");
+        //    sw.WriteLine("pleasure to meet you hello");
+        //    sw.Close();
+
+        //}
+
+        public static void Readforall()
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\user\New Text Document.txt");
+            Console.WriteLine(sr.ReadToEnd());
+            sr.Close();
+        }
+
+        
+
+
+
     }
 }
+
